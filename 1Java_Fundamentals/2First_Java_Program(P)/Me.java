8611dd7428b3dfd1cd6d447b3fc14c17d16eda54
pmar0 <pmarino1994@gmail.com>
2022-07-01 13:42:32 -0400

    initial commit

---
+++ 1Java_Fundamentals/2First_Java_Program(P)/Me.java
@@ -0,0 +1,7 @@
+public class Me {
+    public static void main(String[] args) {
+        System.out.println("My name is Peter");
+        System.out.println("I am 10000 years old :P");
+        System.out.println("My hometown is Brick, NJ");
+    }
+}
